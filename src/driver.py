3c9bbd2e74c82221ba7ef8b19729185412530017
Singh <Harkirat.SINGH@geodis.com>
2023-06-28 13:41:29 -0500

    Initial commit

---
+++ src/driver.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+from heart_disease.src.data import DataExtractor as de
+from heart_disease.src.models import LogisticRegression as lr
+from heart_disease.src.processing import PreProcessor as pre
+
+
